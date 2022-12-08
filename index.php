ebc33b614397d9a824c26b37c0744d35f86d0565
DragiboO <dragibo.scotch@gmail.com>
2022-12-08 12:06:10 +0100

    home

--- index.php
+++ index.php
@@ -1,5 +1,38 @@
 <?php
-include_once 'doctype.template.php';
+include_once 'doctype_template.php';
+?>
+
+<div class="home_quote">
+    <q>LA SITATION</q>
+</div>
+
+<main class="home_main">
+    <section class="home_first_sign">
+        <img src="/assets/img/sign.webp" alt="">
+        <div class="home_first_sign_info">
+            <div>
+                <h2>Titre</h2>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Assumenda atque culpa delectus dignissimos ex nobis ratione vel. Alias at autem cum cumquelaboriosaperferen reprehenderit soluta, veritatis vitae, voluptate voluptatem voluptates voluptatibus voluptatum. Ab, alias aspernatur est eveniet exercitationem incidunt ipsa numquam voluptatibus. Cumque doloremque facere labore sequi vero? Dolor eligendi, enim facere fugiat impedit iste nobis officiis placeat provident repellendus sint.</p>
+            </div>
+            <a href="#">Plus d'information</a>
+        </div>
+    </section>
+
+    <section class="home_other_sign">
+        <div>
+            <h3>Le panneau</h3>
+            <img src="/assets/img/sign.webp" alt="">
+        </div>
+        <div>
+            <h3>Le panneau</h3>
+            <img src="/assets/img/sign.webp" alt="">
+        </div>
+        <div>
+            <h3>Le panneau</h3>
+            <img src="/assets/img/sign.webp" alt="">
+        </div>
+    </section>
+</main>
 
 
 
