0db9e3a29fff36c1854346ad4d844e38334d3f8d
Nalithdev <Ethan.delcroix@outlook.fr>
2022-12-12 11:09:21 +0100

    add page blog and new scrollbar

--- blog.php
+++ blog.php
@@ -1,8 +1,58 @@
 <?php
-require "doctype.template.php";
+require "doctype_template.php";
 ?>
 
+<main>
+    <div class="container">
+        <div class="card">
+            <h3>Bonjour</h3>
+            <img src="img/sign.webp">
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium cumque debitis deserunt ipsam laborum magnam quasi repellendus repudiandae tenetur vero?  </p>
+        </div>
+        <div class="card">
+            <h3>Bonjour</h3>
+            <img src="img/sign.webp">
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Blanditiis consequatur eaque fugit laborum quasi rerum sunt? Enim illum laborum sint?</p>
+        </div>
+        <div class="card">
+            <h3>Bonjour</h3>
+            <img src="img/sign.webp">
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quod, recusandae! </p>
+        </div>
+        <div class="card">
+            <h3>Bonjour</h3>
+            <img src="img/sign.webp">
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quod, recusandae! </p>
+        </div>
+        <div class="card">
+            <h3>Bonjour</h3>
+            <img src="img/sign.webp">
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quod, recusandae! </p>
+        </div>
+        <div class="card">
+            <h3>Bonjour</h3>
+            <img src="img/sign.webp">
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quod, recusandae! </p>
+        </div>
+        <div class="card">
+            <h3>Bonjour</h3>
+            <img src="img/sign.webp">
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quod, recusandae! </p>
+        </div>
+        <div class="card">
+            <h3>Bonjour</h3>
+            <img src="img/sign.webp">
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quod, recusandae! </p>
+        </div>
 
+
+
+    </div>
+
+
+
+
+</main>
 
 
 
--- doctype_template.php
+++ doctype_template.php
@@ -5,8 +5,9 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="/assets/sass/julien.css" />
+
     <link rel="stylesheet" href="Ethan.css" />
+    <link rel="stylesheet" href="/assets/sass/julien.css" />
     <title>Document</title>
 </head>
 <body>
@@ -16,8 +17,8 @@
                 <img src="" alt="logo">
             </a>
             <ul>
-                <li><a href="#">accueil</a></li>
-                <li><a href="#">blog</a></li>
+                <li><a href="index.php">accueil</a></li>
+                <li><a href="blog.php">blog</a></li>
                 <li><a href="#">faq</a></li>
                 <li><a href="#">devis</a></li>
                 <li><a href="#">contact</a></li>
