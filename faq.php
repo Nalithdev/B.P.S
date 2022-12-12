86643ce7b873d060a53d0fa20c9b952394520ea5
MEHDI BELLAM <porcepic91@gmail.com>
2022-12-12 16:07:38 +0100

    ajout de la page faq et ajout du doctype_template dans la page faq

--- faq.php
+++ faq.php
@@ -1,15 +1,7 @@
-<!DOCTYPE html>
-<html lang="en">
-  <head>
-    <meta charset="UTF-8" />
-    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <link rel="stylesheet" href="mehdi-bellam.css" />
-    <link rel="preconnect" href="https://use.typekit.net/pwi7gtt.css">
-    <script src="https://kit.fontawesome.com/dd939f4e1e.js" crossorigin="anonymous"></script>
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css" integrity="sha512-MV7K8+y+gLIBoVD59lQIYicR65iaqukzvf/nwasF0nqhPay5w/9lJmVM2hMDcnK1OnMGCdVK+iQrJ7lzPJQd1w==" crossorigin="anonymous" referrerpolicy="no-referrer" />
-    <title>Document</title>
-  </head>
+<?php
+include_once 'doctype_template.php';
+?>
+
   <body>
     <div class="containerFAQ">
       <div class="questionFAQ questionFAQ1 hidden"  id='1' >
