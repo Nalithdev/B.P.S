a9bddbe47e9d6557bc3085f186259da8a49f620e
DragiboO <dragibo.scotch@gmail.com>
2022-12-12 11:13:31 +0100

    page devis

---
+++ devis.php
@@ -0,0 +1,19 @@
+<?php
+include_once 'doctype_template.php';
+?>
+
+<section class="devis">
+    <h2>devis</h2>
+    <form>
+        <div class="first">
+            <input type="text" placeholder="Nom">
+            <input type="text" placeholder="Prénom">
+            <input type="email" placeholder="E-mail">
+        </div>
+        <div class="second">
+            <textarea name="text"></textarea>
+            <input type="button" value="Envoyer">
+        </div>
+
+    </form>
+</section>
