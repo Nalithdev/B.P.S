431cb18d11cdc2ea41258d9f72f8071223bd00f5
MEHDI BELLAM <porcepic91@gmail.com>
2022-12-13 12:12:33 +0100

    verification du seo pour la page blog.php

--- blog.php
+++ blog.php
@@ -6,42 +6,42 @@ require_once "doctype_template.php";
     <div class="container">
         <div class="card">
             <h3>Bonjour</h3>
-            <img src="img/sign.webp">
+            <img src="img/sign.webp" alt="logo">
             <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium cumque debitis deserunt ipsam laborum magnam quasi repellendus repudiandae tenetur vero?  </p>
         </div>
         <div class="card">
             <h3>Bonjour</h3>
-            <img src="img/sign.webp">
+          <img src="img/sign.webp" alt="logo">
             <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Blanditiis consequatur eaque fugit laborum quasi rerum sunt? Enim illum laborum sint?</p>
         </div>
         <div class="card">
             <h3>Bonjour</h3>
-            <img src="img/sign.webp">
+           <img src="img/sign.webp" alt="logo">
             <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quod, recusandae! </p>
         </div>
         <div class="card">
             <h3>Bonjour</h3>
-            <img src="img/sign.webp">
+         <img src="img/sign.webp" alt="logo">
             <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quod, recusandae! </p>
         </div>
         <div class="card">
             <h3>Bonjour</h3>
-            <img src="img/sign.webp">
+           <img src="img/sign.webp" alt="logo">
             <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quod, recusandae! </p>
         </div>
         <div class="card">
             <h3>Bonjour</h3>
-            <img src="img/sign.webp">
+            <img src="img/sign.webp" alt="logo">
             <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quod, recusandae! </p>
         </div>
         <div class="card">
             <h3>Bonjour</h3>
-            <img src="img/sign.webp">
+            <img src="img/sign.webp" alt="logo">
             <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quod, recusandae! </p>
         </div>
         <div class="card">
             <h3>Bonjour</h3>
-            <img src="img/sign.webp">
+            <img src="img/sign.webp" alt="logo">
             <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quod, recusandae! </p>
         </div>
     </div>
--- doctype_template.php
+++ doctype_template.php
@@ -3,6 +3,7 @@
 <head>
     <meta charset="UTF-8">
    <meta name="viewport" content="width=device-width">
+   
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="mehdi-bellam.css" >
     <link rel="stylesheet" href="Ethan.css" >
